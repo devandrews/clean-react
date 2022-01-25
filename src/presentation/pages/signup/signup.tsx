fffa5d30929b6aecc94d7b45b234cadae3099312
andrews patrick <developer.andrews@gmail.com>
2022-01-24 21:12:53 -0300

    feat: ensure SignUp change status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import Styles from './signup-styles.scss'
-import { Footer, Input, LoginHeader, FormStatus } from '@/presentation/components'
+import {
+  Footer,
+  Input,
+  LoginHeader,
+  FormStatus
+} from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import { Validation } from '@/presentation/protocols/validation'
 
@@ -13,31 +18,57 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
     isLoading: false,
     name: '',
     nameError: '',
-    emailError: 'Campo obrigatório',
-    passwordError: 'Campo obrigatório',
-    passwordConfirmationError: 'Campo obrigatório',
+    email: '',
+    emailError: '',
+    password: '',
+    passwordError: '',
+    passwordConfirmation: '',
+    passwordConfirmationError: '',
     mainError: ''
   })
 
   useEffect(() => {
     setState({
       ...state,
-      nameError: validation.validate('name', state.name)
+      nameError: validation.validate('name', state.name),
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate(
+        'passwordConfirmation',
+        state.passwordConfirmation
+      )
     })
-  }, [state.name])
+  }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   return (
     <div className={Styles.signup}>
       <LoginHeader />
-      <Context.Provider value={ { state, setState }}>
+      <Context.Provider value={{ state, setState }}>
         <form data-testid="form" className={Styles.form}>
           <h2>Criar Conta</h2>
           <Input type="text" name="name" placeholder="Digite seu nome" />
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
-          <Input type="password" name="password" placeholder="Digite sua senha" />
-          <Input type="password" name="passwordConfirmation" placeholder="Repita sua senha" />
-          <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
-          <span data-testid="signup" className={Styles.link}>Voltar para o Login</span>
+          <Input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+          />
+          <Input
+            type="password"
+            name="passwordConfirmation"
+            placeholder="Repita sua senha"
+          />
+          <button
+            data-testid="submit"
+            disabled
+            className={Styles.submit}
+            type="submit"
+          >
+            Entrar
+          </button>
+          <span data-testid="signup" className={Styles.link}>
+            Voltar para o Login
+          </span>
           <FormStatus />
         </form>
       </Context.Provider>
