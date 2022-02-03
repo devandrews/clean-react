f027b21193b57065da5f7a5c1e4c207ce728c7f6
andrews patrick <developer.andrews@gmail.com>
2022-02-03 20:14:05 -0300

    feat: create SignUp composition

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { SignUp } from '@/presentation/pages'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { makeRemoteAddAccount } from '@/main/factories/usecases/remote-add-account/remote-add-account'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/local-save-access-token-factory'
 
 export const makeSignup: React.FC = () => {
   return (
   <SignUp
     validation={makeSignUpValidation()}
+    addAccount={makeRemoteAddAccount()}
+    saveAccessToken={makeLocalSaveAccessToken()}
   />
   )
 }
