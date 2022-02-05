cef911dda7c1c141ed7dabcd72a45010feab8c04
andrews patrick <developer.andrews@gmail.com>
2022-02-05 18:58:09 -0300

    test: ensure Login present error message if something wrong happens

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,6 +1,10 @@
 import faker from 'faker'
 
 const baseUrl: string = Cypress.config().baseUrl
+const xhrRequest = {
+  method: 'POST',
+  url: /login/
+}
 
 describe('Login', () => {
   beforeEach(() => {
@@ -44,23 +48,58 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('Should present error if invalid credentials are provided', () => {
+  it('Should present InvalidCredentialsError on 401', () => {
+    cy.intercept(xhrRequest, {
+      statusCode: 401,
+      response: {
+        error: faker.random.words()
+      }
+    }).as('login')
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.internet.password(5))
     cy.getByTestId('submit').click()
+    cy.wait('@login')
     cy.getByTestId('error-wrap')
-      .getByTestId('spinner').should('exist')
-      .getByTestId('main-error').should('contain.text','Credenciais inválidas')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Credenciais inválidas')
+    cy.url().should('equal', `${baseUrl}login`)
+  })
+
+  it('Should present UnexpectedError on 400', () => {
+    cy.intercept(xhrRequest, {
+      statusCode: 400,
+      response: {
+        error: faker.random.words()
+      }
+    }).as('login')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.internet.password(5))
+    cy.getByTestId('submit').click()
+    cy.wait('@login')
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('not.exist')
+      .getByTestId('main-error')
+      .should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
     cy.url().should('equal', `${baseUrl}login`)
   })
 
   it('Should save accessToken if valid credentials are provided', () => {
+    cy.intercept(xhrRequest, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.random.word()
+      }
+    }).as('login')
     cy.getByTestId('email').focus().type('teste@andrews.com')
     cy.getByTestId('password').focus().type('12345')
     cy.getByTestId('submit').click()
-    cy.getByTestId('error-wrap')
-      .getByTestId('main-error').should('not.exist')
+    cy.wait('@login')
     cy.url().should('equal', `${baseUrl}`)
-    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+    cy.window().then((window) =>
+      assert.isOk(window.localStorage.getItem('accessToken'))
+    )
   })
 })
